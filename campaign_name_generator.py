2b9999dbe995a2ae914af51a22f4acbc387412e8
JaafarMediaplus <110975225+JaafarMediaplus@users.noreply.github.com>
2022-08-23 10:23:25 +0400

    Update campaign_name_generator.py

--- campaign_name_generator.py
+++ campaign_name_generator.py
@@ -93,7 +93,7 @@ product = colproduct.selectbox(
 )
 product_variant = colproductvariant.selectbox(
     'Product Variant/Creative Name:',
-    ('NA', 'SustainableDriving', 'Destination', 'OnOffRoad')
+    ('NA', 'SustainableDriving', 'Destination', 'OnOffRoad', 'EngineNoises')
 )
 
 collanguage, colgender = st.columns(2)
