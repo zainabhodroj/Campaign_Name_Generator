38f824e0f998687527701e7c34222d4da04b54a9
JaafarMediaplus <110975225+JaafarMediaplus@users.noreply.github.com>
2022-11-28 15:11:08 +0400

    Update campaign_name_generator.py

--- campaign_name_generator.py
+++ campaign_name_generator.py
@@ -93,7 +93,7 @@ product = colproduct.selectbox(
     'Product Name:',
     ('NA', 'All', 'WashingMachine', 'System', 'CRM', 'Dueler', 'Potenza', 'Alenza', 'Ecopia', 'ContentHub', 'UGC', 'SME', 'Auto',
      'Polyethylene', 'Polypropylene', 'FoodPackaging', 'SME', 'Toyota', 'Kia', 'Hyundai', 'Geely', 'CashIndividual', 'UsedCars', 'DistractedDriving', 'ClearRoad',
-    'RealEstate', 'Microfinance')
+    'RealEstate', 'Microfinance', 'TodayTab', 'SrchTab', 'SrchResults')
 )
 product_variant = colproductvariant.selectbox(
     'Product Variant/Creative Name:',
